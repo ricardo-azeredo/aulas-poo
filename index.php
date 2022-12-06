11b0361876e27b21e547f8a0f39eb0e58a0139be
Ricardo <ricoazeredo@yahoo.com.br>
2022-12-06 20:09:12 -0300

    Criando a aula 1

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+ include "header.php";
+?>
+
+<body>
+    <header>
+        <h1>Materiais das Aulas de PHP</h1>
+    </header>
+    <main>
+        <div class="slide-links">            
+            <a
+             target="_blank" 
+             href="https://docs.google.com/presentation/d/1njzMWO_BQ2rB19R5U1jsrtLRs4s_oR8glsW0nM6hKU4/edit?usp=sharing"
+            >
+                Programação Orientada à Objeto - Introdução
+            </a>
+           
+        </div>
+    </main>  
+    
+    <a class="oculto" href="exercicios-aula2.php">Exercícios</a>
+</body>
+</html>
